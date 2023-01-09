47d0b0267a9e263817b18c348e8e5e81ecf656cd
Parker Mathis <parkerjmathis@gmail.com>
2023-01-08 17:42:48 -0800

    Added bcrypt hashing for passwords

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -13,6 +13,10 @@ const userData = [
   },
 ];
 
-const seedUsers = () => User.bulkCreate(userData);
+const seedUsers = () =>
+  User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
 
 module.exports = seedUsers;

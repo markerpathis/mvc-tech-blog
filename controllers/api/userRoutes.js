5233a90dba51e05dd0f2425844675ed1d40a9f90
Parker Mathis <parkerjmathis@gmail.com>
2023-01-09 18:05:18 -0800

    Signup process added

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,4 +1,23 @@
-const router = require('express').Router();
-const { User } = require('../../models');
+const router = require("express").Router();
+const { User } = require("../../models");
+
+router.post("/", async (req, res) => {
+  try {
+    console.log("REQ: ", req);
+    console.log("REQ BODY: ", req.body);
+    const dbUserData = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    req.session.save(() => {
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -23,7 +23,21 @@ app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 /////////////////////////////////////////////////////////
 
+// Set up sessions with cookies
+const sess = {
+  secret: "Super secret secret",
+  cookie: {
+    // Stored in milliseconds
+    maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 // app.use(session(sess));
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

fe767637c91db586677a45849d312c03e4e7ff2b
Parker Mathis <parkerjmathis@gmail.com>
2023-01-10 17:27:56 -0800

    Added auth helper

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { User, Post } = require("../models");
+const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   const postData = await Post.findAll().catch((err) => {

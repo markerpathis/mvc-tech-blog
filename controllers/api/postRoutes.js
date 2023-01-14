0376c5affc6de8dca976c234439530a284fad1f1
Parker Mathis <parkerjmathis@gmail.com>
2023-01-13 17:23:25 -0800

    Moved post by id route to posts folder and updated handlebards template

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,4 +1,22 @@
 const router = require("express").Router();
-const { Post } = require("../../models");
+const { Post, User } = require("../../models");
+
+router.get("/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: User,
+    });
+    console.log("postData: ", postData);
+    if (!postData) {
+      res.status(404).json({ message: "No blog post with this id!" });
+      return;
+    }
+    const post = postData.get({ plain: true });
+    console.log("post: ", post);
+    res.render("post-view", { post, logged_in: req.session.logged_in });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -17,24 +17,6 @@ router.get("/", async (req, res) => {
   });
 });
 
-router.get("/post/:id", async (req, res) => {
-  try {
-    const postData = await Post.findByPk(req.params.id, {
-      include: User,
-    });
-    console.log("postData: ", postData);
-    if (!postData) {
-      res.status(404).json({ message: "No blog post with this id!" });
-      return;
-    }
-    const post = postData.get({ plain: true });
-    console.log("post: ", post);
-    res.render("post-view", { ...post, logged_in: req.session.logged_in });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 router.get("/login", (req, res) => {
   res.render("login");
 });

bd4b52233f142f19a57167b00e5b4ba4aabfd5d1
Parker Mathis <parkerjmathis@gmail.com>
2023-01-14 09:58:44 -0800

    Updated post by id route and template to include comments

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,19 +1,32 @@
 const router = require("express").Router();
-const { Post, User } = require("../../models");
+const { Post, User, Comment } = require("../../models");
 
 router.get("/:id", async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id, {
       include: User,
     });
-    console.log("postData: ", postData);
+    // console.log("postData: ", postData);
+
     if (!postData) {
       res.status(404).json({ message: "No blog post with this id!" });
       return;
     }
     const post = postData.get({ plain: true });
     console.log("post: ", post);
-    res.render("post-view", { post, logged_in: req.session.logged_in });
+
+    const commentData = await Comment.findAll({
+      include: User,
+      where: {
+        post_id: post.id,
+      },
+    });
+    // console.log("commentData: ", commentData);
+
+    const comments = commentData.map((comment) => comment.get({ plain: true }));
+    // console.log("comments: ", comments);
+
+    res.render("post-view", { post, comments, logged_in: req.session.logged_in });
   } catch (err) {
     res.status(500).json(err);
   }
--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -2,13 +2,22 @@ const { Comment } = require("../models");
 
 const commentData = [
   {
-    content: "Lorem ipsum dolor sit amet.",
+    content: "1. Lorem ipsum dolor sit amet.",
     user_id: 1,
     post_id: 1,
   },
   {
-    title: "Example Blog Post 2",
-    content: "Nisl suscipit adipiscing bibendum est ultricies.",
+    content: "2. Lorem ipsum dolor sit amet.",
+    user_id: 2,
+    post_id: 1,
+  },
+  {
+    content: "1. Nisl suscipit adipiscing bibendum est ultricies.",
+    user_id: 1,
+    post_id: 2,
+  },
+  {
+    content: "2. Nisl suscipit adipiscing bibendum est ultricies.",
     user_id: 2,
     post_id: 2,
   },

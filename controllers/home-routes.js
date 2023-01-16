61534be78edcb46d33a45032f7e0b9131938cd46
Parker Mathis <parkerjmathis@gmail.com>
2023-01-16 09:32:47 -0800

    Updated homepage and included User on the get all posts route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,12 +3,14 @@ const { User, Post } = require("../models");
 const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
-  const postData = await Post.findAll().catch((err) => {
+  const postData = await Post.findAll({
+    include: User,
+  }).catch((err) => {
     res.json(err);
   });
   // modelName: 'project' (defined in Project.js model file)
   const posts = postData.map((post) => post.get({ plain: true }));
-  // console.log("posts: ", posts);
+  console.log("posts: ", posts);
   // This method is rendering the 'all' Handlebars.js template. This is how we connect each route to the correct template.
   res.render("all", {
     posts,
